6500187b1e310a7e821527a272e3dfcb9fb0df65
hilmimubarok <m.hilmimubarok@gmail.com>
2019-10-10 10:35:29 +0700

    edited to new mysqli

--- simpan.php
+++ simpan.php
@@ -1,14 +1,14 @@
 <?php 
 
-$link    = mysqli_connect("127.0.0.1", "root", "", "tokobuku"); //koneksi
+$conn = new mysqli("localhost", "root", "", "tokobuku"); //koneksi
 
 $judul   = $_POST['judul'];
 $stok    = $_POST['stok'];
 $penulis = $_POST['penulis'];
 
-$result= mysqli_query($link, "insert into barang (judul, stok, penulis) values('$judul','$stok','$penulis')");//create
+$res = $conn->query("insert into barang (judul, stok, penulis) values('$judul','$stok','$penulis')");
 
-if (!$result) {
+if (!$res) {
 	echo "Gagal";
 } else{
 	echo "Berhasil";
